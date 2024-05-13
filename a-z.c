4f1f5501c659ce0d4575075f97b903c767952fd9
SquirrelCorn <145844573+SquirrelCorn@users.noreply.github.com>
2024-05-13 15:16:09 +0000

    Update a-z.c

--- a-z.c
+++ a-z.c
@@ -1,1 +1,3 @@
-#define f(a,b)for(a;++b<26;printf("%c", b + 'a'))#include<stdio.h>int main(){f(int i=-1,i)(i);}
+#define f(a,b)for(a;++b<26;printf("%c", b + 'a'))
+#include<stdio.h>
+int main(){f(int i=-1,i)(i);}
